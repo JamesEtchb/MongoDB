22e138eaaa441e7f2bef094ad37202ab1005efb9
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-21 16:41:08 -0400

    built a js file that imports a library, connects to my mongo/atlis, connected to a specificdatabase, then made a query

---
+++ index.js
@@ -0,0 +1,25 @@
+//import mongo tools / library
+const {MongoClient, ServerApiVersion} = require('mongodb');
+
+//connect to Mongo/atls USING a URL connection string
+const { uri } = require('./dbsecrets.js');
+
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//connect to sample_mflix database
+const db = client.db('sample_mflix');
+
+// Query
+// lets get a specific movie (doc) from the "movies" collection
+// search by title for "Back to the Future"
+
+db.collection('movies')
+.findOne({ title: 'Back to the Future'}, (err, results) => {
+    if(err){
+        console.error(err);
+    }
+    //let's output the results:
+    console.log(results);
+    client.close();
+});
+
